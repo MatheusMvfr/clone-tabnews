edb6be0c84e07bb92591247b0d4ac0ed56f02317
Matheus Vitor <54037468+MatheusMvfr@users.noreply.github.com>
2024-03-22 22:12:53 +0000

    Primeira tentativa de fazer uma página de construção do site

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
+import React from "react";
+
 function Home() {
-  return <h1>Parabéns Daniel!!</h1>;
+  return (
+    <div>
+      <h1>Dubitare</h1>
+      <h2>
+        Bem vindo ao portfólio de Matheus Vitor local onde serão realizados
+        explorações de dados de diferentes tipos buscando encontrar insights
+        construtivos
+      </h2>
+      <img
+        src="/images/programador_em_construcao.jpg"
+        alt="Programador construindo o site"
+      />
+    </div>
+  );
 }
 
 export default Home;
