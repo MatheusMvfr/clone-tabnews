a7631301bb468c84ff19edf16bc3b82e4091f2af
Matheus Vitor <54037468+MatheusMvfr@users.noreply.github.com>
2024-03-23 14:25:14 +0000

    feat(front): Incluindo a descrição do site e sua tela de construção.

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,21 @@
 import React from "react";
+import styles from "./styles.module.css";
 
 function Home() {
   return (
-    <div>
-      <h1>Dubitare</h1>
-      <h2>
-        Bem vindo ao portfólio de Matheus Vitor local onde serão realizados
-        explorações de dados de diferentes tipos buscando encontrar insights
-        construtivos
+    <div className={styles.container}>
+      <h1 className={styles.title}>Dubitare</h1>
+      <h2 className={styles.subtitle}>
+        Bem vindo ao portfólio de Matheus Vitor
       </h2>
+      <p>
+        Essa página será focada na exploração de dados relevantes para o meu
+        crescimento profissional e possívelmente para o seu negócio!
+        <br />
+        Atualmente em construção...
+      </p>
       <img
+        className={styles.image}
         src="/images/programador_em_construcao.jpg"
         alt="Programador construindo o site"
       />
